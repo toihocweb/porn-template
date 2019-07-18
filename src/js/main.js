cd10c2efda50e3c57107f22fbffe024f06686b6f
Nhat <nhat.bui@int.easycredit.vn>
2019-07-18 17:29:01 +0700

    a

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,9 @@
-$(document).ready(function() {
-});
+console.clear();
+
+Splitting({
+  target: '.tiler',
+  by: 'cells',
+  rows: 3,
+  columns: 3,
+  image: true
+}); //Image('.tiler-overlay', { rows: 3, cols: 3 });
