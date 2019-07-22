b3c65f48c7f853b47102aa4db32956b0659b4f47
Nhat <nhat.bui@int.easycredit.vn>
2019-07-22 09:31:21 +0700

    a

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,48 @@
-console.clear();
+let containerTabs = document.getElementById("tabs");
 
-Splitting({
-  target: '.tiler',
-  by: 'cells',
-  rows: 3,
-  columns: 3,
-  image: true
-}); //Image('.tiler-overlay', { rows: 3, cols: 3 });
+// get info Tabs
+const getInfoTabs = container =>{
+	return [...container.querySelectorAll(".tabs__content__item")];
+}
+
+//get links tab
+const getLinksTab = container =>{
+	return [...container.querySelectorAll("a[data-tab]")];
+}
+
+
+
+const activeTab = (linktab,container) => {
+		let tabsInfo = getInfoTabs(container),
+				LinksTab = getLinksTab(container);
+		
+	LinksTab.map(info => info.classList.remove("active-link"));
+		tabsInfo.map(info => info.classList.remove("active-tab"));
+		tabsInfo.map(info => {
+				if(linktab.getAttribute("data-tab") === info.getAttribute("id")){
+					info.classList.add("active-tab");
+					linktab.classList.add("active-link");
+					
+				}
+				
+			
+			
+		})
+
+	
+	
+}
+
+
+const getLinkTab = e => {
+		let target = e.target;
+		if(target.tagName === "A"){
+				activeTab(target,containerTabs);
+		}	
+}
+
+
+containerTabs.addEventListener("click", getLinkTab);
+
+
+
